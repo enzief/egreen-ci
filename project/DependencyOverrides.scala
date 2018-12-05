8516c84126034005c33ed458fb1a27c12c1757e6
Ngoc Duy (Yui) Pham <duyp@cakesolutions.net>
2018-12-05 18:49:47 +0000

    wip

--- project/DependencyOverrides.scala
+++ project/DependencyOverrides.scala
@@ -1,6 +1,6 @@
-package com.round
+package trialblaze
 
-import com.round.Dependencies._
+import trialblaze.Dependencies._
 import sbt._
 
 /**
@@ -9,7 +9,6 @@ import sbt._
 object DependencyOverrides {
 
   val settings: Seq[ModuleID] = Seq(
-    commonsIo,
     typesafeConfig,
     slf4j,
     Akka.actor,
@@ -29,6 +28,7 @@ object DependencyOverrides {
     Netty.codec,
     Netty.handler,
     ScalaPB.grpc,
-    ScalaPB.runtime
+    ScalaPB.runtime,
+    Scalaz.core
   )
 }
--- project/GrpcPlugin.scala
+++ project/GrpcPlugin.scala
@@ -1,16 +1,31 @@
-package com.round
+package trialblaze
 
-import sbt._
-import sbt.Keys._
 import org.lyranthe.fs2_grpc.java_runtime.sbt_gen.Fs2Grpc
 import org.lyranthe.fs2_grpc.java_runtime.sbt_gen.Fs2GrpcPlugin.autoImport._
+import sbt._
+import sbt.Keys._
+import sbtprotoc.ProtocPlugin
+import sbtprotoc.ProtocPlugin.autoImport.PB
 
 object GrpcPlugin extends AutoPlugin {
 
   override def requires: Plugins = Fs2Grpc
-  override def trigger           = allRequirements
+  override def trigger = allRequirements
 
-  override val projectSettings: Seq[Def.Setting[_]] = Seq(
-    scalapbCodeGeneratorOptions += CodeGeneratorOption.FlatPackage
-  )
+  import CodeGeneratorOption._
+  override def projectSettings: Seq[Def.Setting[_]] =
+    Project.inConfig(Test)(ProtocPlugin.protobufConfigSettings) ++
+    Seq(
+      Compile / excludeFilter := HiddenFileFilter || scalapbFilter,
+      Test / PB.protoSources  := Seq(sourceDirectory.value / "test/protobuf"),
+      Test / PB.targets       := Seq(scalapb.gen() -> sourceManaged.in(Test).value),
+      scalapbCodeGeneratorOptions += FlatPackage
+    )
+
+  private def scalapbFilter: io.FileFilter = {
+    val r = "scalapb".r
+    new io.SimpleFileFilter(
+      f => r.findFirstIn(f.getCanonicalPath).nonEmpty
+    )
+  }
 }
